9832664af2dcb4525c1984b7422a533edb9c0df9
hello9597 <43660736+hello9597@users.noreply.github.com>
2023-08-13 03:54:13 +0800

    搜尋頁接API

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,5 +1,5 @@
 // SearchPage.tsx
-import React from 'react';
+import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -7,20 +7,69 @@ import Button from '@mui/material/Button';
 import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import { Link } from 'react-router-dom';
+import { useRandomKeyword, useRandomOpinion, useSearchOpinionByKeyword, useSearchOpinionByOpinion } from '@/services/query';
 
 
+const SearchPage = () => {
 
-function SearchPage() {
-  
-  const [value, setValue] = React.useState('1');
+  const [value, setValue] = useState('1');
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue((event.target as HTMLInputElement).value);
   };
+
+  const [keyword, setKeyword] = useState('');
+  const handleKeywordChange = (event: any) => {
+    if (event.target.value != undefined) {
+      setKeyword(event.target.value);
+    }
+  };
+  const handleRandomKeyWord = () => {
+    refetchKeyword();
+    if (randomKeyword?.keyword != undefined) {
+      setKeyword(randomKeyword?.keyword);
+    }
+  }
+
+  const [opinion, setOpinion] = useState('');
+  const handleOpinionChange = (event: any) => {
+    if (event.target.value != undefined) {
+      setOpinion(event.target.value);
+    }
+  };
+  const handleRandomOpinion = () => {
+    refetchOpinion();
+    if (randomOpinion?.opinion != undefined) {
+      setOpinion(randomOpinion?.opinion);
+    }
+  }
+
+  const handleSearch = () => {
+    if (value == '1') {
+      handleSearchKeyword();
+    }
+    else {
+      handleSearchOpinion();
+    }
+  }
+  const handleSearchKeyword = () => {
+    refetchSearchKeyword();
+    console.log(resultkeyword);
+  }
+  const handleSearchOpinion = () => {
+    refetchSearchOpinion();
+    console.log(resultopinion);
+  }
+
+  const { data: randomKeyword, refetch: refetchKeyword } = useRandomKeyword();
+  const { data: randomOpinion, refetch: refetchOpinion } = useRandomOpinion();
+  const { data: resultkeyword, refetch: refetchSearchKeyword } = useSearchOpinionByKeyword(keyword);
+  const { data: resultopinion, refetch: refetchSearchOpinion } = useSearchOpinionByOpinion(opinion);
+
   return (
     <div>
-      <Box 
-        display="flex" 
-        justifyContent="center" 
+      <Box
+        display="flex"
+        justifyContent="center"
         alignItems="flex-start"
         pt={8}
       >
@@ -34,30 +83,35 @@ function SearchPage() {
             <FormControlLabel value="1" control={<Radio />} label="關鍵字搜尋" />
             <div>
               <TextField
-                style={{width: '745px'}}
-                disabled={value=='2'}
+                style={{ width: '745px' }}
+                disabled={value == '2'}
                 variant="outlined"
                 size="small"
+                value={keyword}
+                onChange={handleKeywordChange}
               />
-              <Button variant="outlined" disabled={value=='2'}>隨機關鍵字</Button>
+              <Button onClick={handleRandomKeyWord} variant="outlined" disabled={value == '2'}>隨機關鍵字</Button>
             </div>
             <FormControlLabel value="2" control={<Radio />} label="見解搜尋" />
             <div>
               <textarea
-                style={{width: '745px'}}
-                disabled={value=='1'}
+                style={{ width: '745px' }}
+                disabled={value == '1'}
                 rows={7}
+                value={opinion}
+                onChange={handleOpinionChange}
               >
               </textarea>
-              <Button style={{width: '100px'}} variant="outlined" disabled={value=='1'}>隨機見解</Button>
+              <Button style={{ width: '100px' }} onClick={handleRandomOpinion} variant="outlined" disabled={value == '1'}>隨機見解</Button>
             </div>
           </RadioGroup>
           <Link to={'/result'}>
             <Button
-              style={{width : '100px'}}
+              style={{ width: '100px' }}
               variant="contained"
               color="success"
               endIcon={<SendIcon />}
+              onClick={handleSearch}
             >
               送出
             </Button>
@@ -69,7 +123,3 @@ function SearchPage() {
 }
 
 export default SearchPage;
-function setValue(value: string) {
-  throw new Error('Function not implemented.');
-}
-
