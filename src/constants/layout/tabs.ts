b7e1745a99b60ff3f6f2f3f7625159bc1e41c43f
eddie <eddie605134@gmailcom>
2023-08-12 22:29:44 +0800

    修改tab文字

--- src/constants/layout/tabs.ts
+++ src/constants/layout/tabs.ts
@@ -6,7 +6,7 @@ interface Tab  {
 }
 
 export const tabs = [
-  { label: "系統介紹＆說明", zIndex: 4, index: 0, path: "/map" },
+  { label: "地圖查詢", zIndex: 4, index: 0, path: "/map" },
   { label: "查詢", zIndex: 3, index: 1, path: "/search" },
   { label: "查詢結果", zIndex: 2, index: 2, path: "/result" },
 ]; 
