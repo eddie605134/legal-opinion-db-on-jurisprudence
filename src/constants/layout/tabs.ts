45a6a47ec2d1cd5215acce0a9e3536d3a71da952
eddie.chang <eddie605134@gmail.com>
2023-08-22 14:12:22 +0800

    搜尋結果文字改成相關見解結果

--- src/constants/layout/tabs.ts
+++ src/constants/layout/tabs.ts
@@ -8,5 +8,5 @@ export interface Tab  {
 export const tabs = [
   { label: "常用見解", zIndex: 4, index: 0, path: "/map" },
   { label: "搜尋見解", zIndex: 3, index: 1, path: "/search" },
-  { label: "搜尋結果", zIndex: 2, index: 2, path: "/result" },
+  { label: "相關見解結果", zIndex: 2, index: 2, path: "/result" },
 ]; 
